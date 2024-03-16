e446c4df2786f33ccdf932c80b4a9d257ea79d93
DivyaMaddipudi <30898620+DivyaMaddipudi@users.noreply.github.com>
2024-03-16 10:59:15 -0700

    Added: log4j2 with config file

---
+++ spring-core/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+//        System.out.println("Hello");
+        logger.info("Application started");
+
+    }
+}
